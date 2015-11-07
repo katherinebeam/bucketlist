a6ef88cd20c5539b386449988410e64ea66ba21e
Katherine Beam <k.beam@umiami.edu>
2015-11-07 13:27:21 -0500

    Deleted gravtastic gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
-  # include Gravtastic
-  # gravtastic
   has_secure_password
   validates :email, presence: true
   validates :email, uniqueness: true
