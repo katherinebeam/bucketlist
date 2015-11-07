544e4cf972977bc81500e8c258010cf94daafc8a
Katherine Beam <k.beam@umiami.edu>
2015-11-07 12:37:21 -0500

    Updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151106220127) do
 
   create_table "adventure_items", force: :cascade do |t|
@@ -36,6 +35,10 @@ ActiveRecord::Schema.define(version: 20151106220127) do
     t.string   "username"
     t.string   "location"
     t.string   "picture"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
