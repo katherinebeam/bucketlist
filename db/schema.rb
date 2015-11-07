51632a8a3b6659eb477b86a685296b69dc133f0d
Arielle Dickey <allouis0059@gmail.com>
2015-11-07 12:33:30 -0500

    gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151106220127) do
 
   create_table "adventure_items", force: :cascade do |t|
@@ -35,6 +34,10 @@ ActiveRecord::Schema.define(version: 20151106220127) do
     t.datetime "updated_at",          null: false
     t.string   "username"
     t.string   "location"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.string   "picture"
   end
 
