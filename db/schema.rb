9bb908e14b598989a18f1a256cc797dc94f47b26
Arielle Dickey <allouis0059@gmail.com>
2015-11-07 12:19:55 -0500

    Took out paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105194222) do
+ActiveRecord::Schema.define(version: 20151106025200) do
 
   create_table "adventure_items", force: :cascade do |t|
     t.string   "title"
@@ -30,10 +30,14 @@ ActiveRecord::Schema.define(version: 20151105194222) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "username"
     t.string   "location"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
